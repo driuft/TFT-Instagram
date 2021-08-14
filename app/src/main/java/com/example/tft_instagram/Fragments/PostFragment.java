918048e9becd069969cd351486d97803ac84bbc0
Aldo Socarras <air.socarras@gmail.com>
2021-08-13 22:59:53 -0400

    Fixed issue with emulator external camera

--- app/src/main/java/com/example/tft_instagram/Fragments/PostFragment.java
+++ app/src/main/java/com/example/tft_instagram/Fragments/PostFragment.java
@@ -126,7 +126,7 @@ public class PostFragment extends Fragment {
         // wrap File object into a content provider
         // required for API >= 24
         // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
-        Uri fileProvider = FileProvider.getUriForFile(context, "com.example.fbu_instagram", pictureFile);
+        Uri fileProvider = FileProvider.getUriForFile(context, "com.example.tft_instagram", pictureFile);
         intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
 
         // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
